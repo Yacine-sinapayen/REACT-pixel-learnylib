aa10266f3e8c0204a7bac53c3dc90b5427494504
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-05-27 15:44:50 +0200

    editAction bug

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AddAction from './Components/AddAction/AddAction';
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment'
+import { responsiveProperty } from '@mui/material/styles/cssUtils';
+import { useResolvedPath } from 'react-router-dom';
 // import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -29,13 +31,13 @@ function App() {
     await fetch(`https://squedio.com/marketing/api/v1/actions`, {
       method: 'POST',
       body: JSON.stringify({
-        id : uuidv4(),
+        id: uuidv4(),
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
         title: title,
         media: media,
         tags: tags,
         target_url: target_url,
         shipments: shipments,
-        created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
       }),
       headers: {
         "Content-type": "application/json; charset=UTF+8",
@@ -56,9 +58,51 @@ function App() {
       })
   };
 
-  // Supression
-  const DeleteAction = async (id) => {
-    
+ 
+// Mofifier action
+  const EditAction = async (id,title, media, tags, target_url, shipments) => {
+    await fetch(`https://squedio.com/marketing/api/v1/actions/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id:uuidv4(),
+        title: title,
+        media: media,
+        tags: tags,
+        target_url: target_url,
+        shipments: shipments
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF+8",
+      }
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          return;
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        
+        const updateActions = actions.map((action) => {
+          if (action.id === id) {
+            action.title = title;
+            action.media = media;
+            action.tags = tags;
+            action.target_url = target_url;
+            action.shipments = shipments;
+          }
+          return action;
+        });
+        setActions((actions) => [...actions, data]);
+        setActions((actions) => updateActions);
+      })
+      .catch((error) => console(error));
+  }
+
+   // Supression
+   const DeleteAction = async (id) => {
+
     let copy = [...actions]
     copy = copy.filter(action => action.id !== id)
     setActions(copy);
@@ -66,26 +110,19 @@ function App() {
     await fetch(`https://squedio.com/marketing/api/v1/actions/${id}`, {
       method: 'DELETE'
     })
-    .then((res) => {
-     console.log(res)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
-
-  const editAction = async (id) => {
-    await fetch(`https://squedio.com/marketing/api/v1/actions/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ title:''})
-    })  
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   return (
     <>
       <Navbar />
       <AddAction CreateAction={CreateAction} />
-      <Action actions={actions} setActions={setActions} DeleteAction={DeleteAction}/>
+      <Action actions={actions} setActions={setActions} DeleteAction={DeleteAction} EditAction={EditAction} />
     </>
   );
 }
--- src/Components/Action/Action.js
+++ src/Components/Action/Action.js
@@ -1,52 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Action.css';
 
-export default function Action(props) {
+export default function Action(props, { EditAction, id }) {
+
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleEdit = () => {
+        setIsEdit(!isEdit);
+    };
+
+    const handleOnEditSubmit = (e) => {
+        e.preventDefault();
+        props.EditAction(id, e.target.title.value,
+            e.target.media.value,
+            e.target.tags.value,
+            e.target.target_url.value);
+        setIsEdit(!isEdit);
+    };
 
     return (
         <>
-            <table className="tableau-style">
-                <thead>
-                    <tr>
-                        <th>Nom de l'action ok</th>
-                        <th>Média ok</th>
-                        <th>Mots clés ok</th>
-                        <th>Url Cible</th>
-                        <th>Nbs d'envois ok</th>
-                        <th>Clic</th>
-                        <th>Url de tracking</th>
-                        <th>Compte créés</th>
-                        <th>Inscriptions</th>
-                        <th>Valeur générée</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody >
-                    {
-                        props.actions.map(({id, title, media, tags, target_url, shipments, clicks, new_accounts, enrollments, value}
-                        ) =>
-                            <tr key={id}>
-                                <td>{title}</td>
-                                <td> {media}</td>
-                                <td> {tags}</td>
-                                <td><a href="{target_url}">{target_url}</a></td>
-                                <td>{shipments}</td>
-                                <td>{clicks}</td>
-                                <td>https://formations.learnylib.com/st/{id}</td>
-                                <td>{new_accounts}</td>
-                                <td>{enrollments}</td>
-                                <td>{value}</td>
-                                <td>
-                                    <button>edit</button>
-                                    <button  
-                                    onClick={() => props.DeleteAction(id)}
-                                    >delete
-                                    </button>
-                                </td>
-                            </tr>
-                        )}
-                </tbody>
-            </table>
+            {isEdit ? (
+                <form onSubmit={handleOnEditSubmit}>
+                    <input type='text' maxLength='200' required={true} placeholder="Nom de l'action" name="title" defaultValue={props.title} />
+                    <input type='text' maxLength='200' required={true} placeholder="Média" name="media" defaultValue={props.media} />
+                    <input type='text' maxLength='200' placeholder="Mot clés" name="tags" defaultValue={props.tags} />
+                    <input type='text' maxLength='200' required={true} placeholder="Url cible" name="target_url" defaultValue={props.target_url} />
+                    <input type='number' min='0' step='1' required={true} placeholder="Nb d'envois" name="shipments" defaultValue={props.shipments} />
+                    <button onSubmit={handleOnEditSubmit}>Save</button>
+                </form>
+            ) : (
+                <table className="tableau-style">
+                    <thead>
+                        <tr>
+                            <th>Nom de l'action ok</th>
+                            <th>Média ok</th>
+                            <th>Mots clés ok</th>
+                            <th>Url Cible</th>
+                            <th>Nbs d'envois ok</th>
+                            <th>Clic</th>
+                            <th>Url de tracking</th>
+                            <th>Compte créés</th>
+                            <th>Inscriptions</th>
+                            <th>Valeur générée</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody >
+                        {
+                            props.actions.map(({ id, title, media, tags, target_url, shipments, clicks, new_accounts, enrollments, value }
+                            ) =>
+                                <tr key={id}>
+                                    <td>{title}</td>
+                                    <td> {media}</td>
+                                    <td> {tags}</td>
+                                    <td><a href="{target_url}">{target_url}</a></td>
+                                    <td>{shipments}</td>
+                                    <td>{clicks}</td>
+                                    <td>https://formations.learnylib.com/st/{id}</td>
+                                    <td>{new_accounts}</td>
+                                    <td>{enrollments}</td>
+                                    <td>{value}</td>
+                                    <td>
+                                        <button onClick={handleEdit}>Edit</button>
+                                        {/* <button onClick={() => props.EditAction(id)} >edit</button> */}
+                                        <button
+                                            onClick={() => props.DeleteAction(id)}
+                                        >delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            )}
+                    </tbody>
+                </table>
+            )}
         </>
     )
 }
