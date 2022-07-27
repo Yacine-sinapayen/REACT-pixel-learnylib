5e3fdc8a5ace567ea9eb6381d466fecf553cd5eb
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-07-27 10:40:04 +0200

    reprise taille btn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./context/userContext";
+import App from "./App";
 import "./scss/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/userContext";
-import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import SignInForm from "../../components/SignInForm/SignInForm";
 import PrivateActions from "../Home/PrivateActions/PrivateActions";
 
@@ -10,9 +9,7 @@ export default function Home() {
 
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">
         {currentUser ? <PrivateActions /> : <SignInForm />}
-      </h1>
     </div>
   );
 };
